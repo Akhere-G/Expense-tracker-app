79f46d952c2a2348c73d7bff5f9818af39b62af5
Akhere Ihoeghinlan <zcabaih@ucl.ac.uk>
2020-12-13 20:49:56 +0000

    added styles to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,17 +5,28 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className={styles.header}>
-      <nav>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/about'>About</Link>
-        </li>
-        <li>
-          <Link to='/contact'>Contact</Link>
-        </li>
+    <header className={styles.container}>
+      <nav className={styles.center}>
+        <div className={styles.logo}>
+          <h4>Income Tracker</h4>
+        </div>
+        <ul className={styles.links}>
+          <li>
+            <Link to='/' className={styles.link}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to='/about' className={styles.link}>
+              About
+            </Link>
+          </li>
+          <li>
+            <Link to='/contact' className={styles.link}>
+              Contact
+            </Link>
+          </li>
+        </ul>
       </nav>
     </header>
   );
