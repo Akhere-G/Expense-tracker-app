43de0efa2b9a4c32d3c50887ab9945412aeddb13
Akhere Ihoeghinlan <zcabaih@ucl.ac.uk>
2020-12-14 17:14:57 +0000

    minor style changes

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Search = () => {
-  return <div>s</div>;
+  return (
+    <div className={`mainBox`}>
+      <h4>Filter</h4>
+    </div>
+  );
 };
 
 export default Search;
