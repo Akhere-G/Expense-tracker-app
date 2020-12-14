2fcf0693ebcd7098463184780ac37d97f572f510
Akhere Ihoeghinlan <zcabaih@ucl.ac.uk>
2020-12-14 23:12:13 +0000

    separated selector

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,72 +1,22 @@
 import React, { useState } from "react";
 import styles from "./Search.module.css";
-import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
+import Selector from "./Selector/Selector";
 import useGlobalState from "../../context";
 
-const selectOptions = {
-  default: "all",
-  options: ["all", "income", "expenses"],
-};
+const options = ["all", "income", "expenses"];
 const Search = () => {
-  const [option, setOption] = useState(selectOptions.default);
-  const [showMenu, setShowMenu] = useState(false);
   const { changeSearchOption } = useGlobalState();
 
-  const openMenu = () => {
-    setShowMenu(true);
-  };
-
-  const toggleMenu = () => {
-    setShowMenu(!showMenu);
-  };
-
-  const closeMenu = () => {
-    setShowMenu(false);
-  };
-
-  const handleBlur = e => {
-    if (e.target.id === "last") {
-      setShowMenu(false);
-    }
-  };
-
+  const [option, setOption] = useState(options[0]);
   const choseOption = newOption => {
     console.log("switched to: ", newOption);
     setOption(newOption);
     changeSearchOption(newOption);
-    closeMenu();
   };
-
   return (
     <div className={`mainBox ${styles.search}`}>
-      <h4>Filter</h4>
-      <div
-        className={styles.selector}
-        onBlurCapture={e => {
-          handleBlur(e);
-        }}
-      >
-        <button className={styles.current} onClick={toggleMenu}>
-          <p>{option}</p>
-          <KeyboardArrowDownIcon />
-        </button>
-
-        <div className={`${styles.dropdown} ${showMenu ? styles.show : ""}`}>
-          {selectOptions.options.map((option, index) => {
-            return (
-              <button
-                disabled={!showMenu}
-                key={index}
-                className={styles.option}
-                id={selectOptions.options.length - 1 === index ? "last" : ""}
-                onClick={() => choseOption(option)}
-              >
-                {option}
-              </button>
-            );
-          })}
-        </div>
-      </div>
+      <h4>Sort Transactions</h4>
+      <Selector choseOption={choseOption} value={option} options={options} />
     </div>
   );
 };
---
+++ src/components/Search/Selector/Selector.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
+
+import styles from "./Selector.module.css";
+const Selector = ({ value, choseOption, options }) => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const openMenu = () => {
+    setShowMenu(true);
+  };
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const closeMenu = () => {
+    setShowMenu(false);
+  };
+
+  const handleBlur = e => {
+    if (e.target.id === "last") {
+      setShowMenu(false);
+    }
+  };
+
+  return (
+    <div
+      className={styles.selector}
+      onBlurCapture={e => {
+        handleBlur(e);
+      }}
+    >
+      <button className={styles.current} onClick={toggleMenu}>
+        <p>{value}</p>
+        <KeyboardArrowDownIcon />
+      </button>
+
+      <div className={`${styles.dropdown} ${showMenu ? styles.show : ""}`}>
+        {options.map((option, index) => {
+          return (
+            <button
+              disabled={!showMenu}
+              key={index}
+              className={styles.option}
+              id={options.length - 1 === index ? "last" : ""}
+              onClick={() => {
+                choseOption(option);
+                closeMenu();
+              }}
+            >
+              {option}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Selector;
